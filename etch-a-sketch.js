258dd379717dc4a84953ff48f8877a06e245001a
Ivan Khoo <ivanakhoo@gmail.com>
2024-12-29 01:10:54 -0500

    Created grid and hover effect

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,24 @@
 // Create 16 div by 16 div
 
-// Practice with hover event
+function genDiv(num) {
+    const container = document.querySelector(".container");
+    for (var i = 0; i < num; i++) {
+        for (var j = 0; j < num; j++) {
+            const div = document.createElement("div");
+            div.style.border = "1px solid black";
+            div.className = "aloha";
+            const size = 320 / num;
+            div.style.width = `${size}px`; 
+            div.style.height = `${size}px`;
+
+            div.addEventListener("mouseenter", () => {
+                div.classList.toggle("highlighted");
+            });
+
+            container.appendChild(div);
+        }
+    }
+}
+
+genDiv(16);
 
